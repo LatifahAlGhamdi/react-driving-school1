5f73ae5157586399182f55b3dfbc01fbeb1d33fd
LatifahAlGhamdi <92169836+LatifahAlGhamdi@users.noreply.github.com>
2022-01-19 18:10:20 +0300

    +

---
+++ src/utils/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase"
+const firebaseConfig = {
+  apiKey: "AIzaSyDsrJFh7TH2-ytOScHuzG1IfoNzHRXfiU0",
+  authDomain: "drivingschool-4b5a6.firebaseapp.com",
+  projectId: "drivingschool-4b5a6",
+  storageBucket: "drivingschool-4b5a6.appspot.com",
+  messagingSenderId: "643616606710",
+  appId: "1:643616606710:web:0c790f907551380399dfd7",
+  measurementId: "G-XQ8GG0ZMX6"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+export default firebase;
